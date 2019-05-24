ef3ae2a58145e736759bdd95ea1b5c88886ca93b
Sam Schott <ss2151@cam.ac.uk>
2019-05-24 18:18:51 +0100

    bug fixes

--- utils/get_varargin.m
+++ utils/get_varargin.m
@@ -1,17 +1,19 @@
-function [argout, input] = get_varargin(input, name, default)
+function [returnValue, output] = get_varargin(input, name, defaultValue)
 %GET_VARARGIN Pairs up argument discriptions with values form varargin.
 
-for i = 1:length(input)
-    if ischar(input{i}) && strcmp(input{i}, name)
-        argout = input{i+1};
-        input(:,i+1) = [];
-        input(:,i) = [];
+output = input;
+
+for i = 1:length(output)
+    if ischar(output{i}) && strcmp(output{i}, name)
+        returnValue = output{i+1};
+        output(:,i+1) = [];
+        output(:,i) = [];
         return
     end
 end
 
 if ~exist('argout', 'var') && nargin == 3
-    argout = default;
+    returnValue = defaultValue;
 end
 
 end
