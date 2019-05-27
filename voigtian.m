cb1394b8b2d1792d662b287245db614142883dd8
Sam Schott <ss2151@cam.ac.uk>
2019-05-27 16:47:41 +0100

    allow calculation of n-th derivative voigtian
    this creates feature parity between ‘voigtian’ and ‘lorentzian’

--- voigtian.m
+++ voigtian.m
@@ -1,5 +1,5 @@
 function [y] = voigtian(x, x0, FWHM_gauss, FWHM_lorentz, varargin)
-%VOIGTIAN calculates a Voight profile or its 1st derivative.
+%VOIGTIAN calculates a Voight profile or its n-th derivative.
 %
 %   The area of the Voigtian is normalized to 1.
 %
@@ -25,16 +25,15 @@ deriv = get_varargin(varargin, 'deriv', 0);
 
 % padding with additional values for derivative
 N = length(x); dx=mean(diff(x));
-if deriv==0
+if deriv == 0
     xNew = x;
-elseif deriv==1
-    xNew(2:N+1) = x;
-    xNew(1) = x(1)-dx;
-    xNew(N+2) = x(N)+dx;
+else
+    xNew(1+deriv:N+deriv) = x;
+    xNew(1:deriv) = x(1)-dx*(deriv:-1:1);
+    xNew(N+deriv+1:N+2*deriv) = x(N)+dx*(1:deriv);
 end 
 
-% converting to dimensionless coordinates
-
+% convert to dimensionless coordinates and calculate voigtian from fadf
 HWHM_lorentz = FWHM_lorentz/2;
 HWHM_gauss = FWHM_gauss/2;
 
@@ -44,10 +43,11 @@ Imag = sqrt(log(2)).*(HWHM_lorentz/HWHM_gauss);
 w = fadf(Real+1j*Imag);
 y = sqrt(log(2)/pi)/HWHM_gauss.*real(w);
 
-% calculate first derivative
-if deriv==1
+% calculate n-th derivative numerically
+while deriv > 0
     yDeriv = (y(3:N+2)-y(1:N))/(2*dx);
     y = yDeriv;
+    deriv = deriv - 1;
 end
 
 y = reshape(y, size(x));
