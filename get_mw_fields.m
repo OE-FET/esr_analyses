acbd171a9735d51df0c18c15c9e9019ff261f030
Sam Schott <ss2151@cam.ac.uk>
2019-10-09 23:42:55 +0100

    add calibration as keyword argument

--- get_mw_fields.m
+++ get_mw_fields.m
@@ -1,14 +1,19 @@
-function Bmw = get_mw_fields(pars)
+function Bmw = get_mw_fields(pars, varargin)
 %GET_MW_FIELDS Gets microwave fields from measurement parameter structure.
 %
-%   Converts MW powers given in pars structure to MW magentic field
+%   Converts MW powers given in pars structure to MW magnetic field
 %   amplitudes, given the measurement conditions from pars. In case of
 %   power saturation measurements, a vector is returned. Otherwise,
 %   GET_MW_FIELDS returns a scalar.
 %
+% 	SYNTAX:
+% 	Bmw = GET_MW_FIELDS(pars)
+% 	Bmw = GET_MW_FIELDS(pars, 'cal', 2.2)
+%
 %   INPUT(S):
-%   pars - structure containing measurement conditions, sample dimensions,
-%          etc.
+%   pars  - structure containing measurement conditions, sample dimensions, etc.
+%   'cal' - calibration factor for cavity, defaults to 2.86 to Bruker SHQE cavity
+%           Oxford Instruments ESR900 cryostat under vacuum
 %
 %   OUTPUT(S):
 %   Bmw  - Microwave magnetic field amplitude(s) in Tesla
@@ -27,14 +32,13 @@ else
     Pmw = pars.MWPW; % MW Power in W
 end
 
-% convert MWPW to magnetic field strength in Tesla
-% use Qref = 7500 and c = 2.0 for Nagoya files
+cal  = get_kwarg(varargin, 'cal', 2.2*1.3);
 Qref    = 8355;
-c       = 2.2;      % without cryostat mounted
-cCryo   = 2.2*1.3;  % with crystat, calibrated with gMarker
+
+% convert MWPW to magnetic field strength in Tesla
 
 pars    = get_sample_position(pars);
 f_mean  = mw_mean(pars);
-Bmw     = f_mean * cCryo * sqrt(Pmw) * sqrt(pars.QValue/Qref) * 1E-4; % in Tesla
+Bmw     = f_mean * cal * sqrt(Pmw) * sqrt(pars.QValue/Qref) * 1E-4; % in Tesla
 
 end
