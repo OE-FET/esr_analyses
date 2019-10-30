3855c42a161f533d1a005dde983fe6c46daada3f
Sam Schott <ss2151@cam.ac.uk>
2019-10-30 13:37:46 +0000

    bug fixes

--- phase_cycle.m
+++ phase_cycle.m
@@ -14,7 +14,7 @@ function [s0, s90, phase_shift] = phase_cycle(sig_x, sig_y, varargin)
 %   INPUT(S):
 %   sig_x - X-channel from lock-in.
 %   sig_y - Y-channel from lock-in.
-%   phase - Phase offset in rad to use. If not given, the appropriate 
+%   phase - Phase offset in rad to use. If not given, the appropriate
 %           phase will be determined by minimizing the out-of-phase signal.
 %   plot  - If true, the original and phase-corrected signals will be
 %           plotted.
@@ -31,11 +31,11 @@ phase_shift = get_kwarg(varargin, 'phase', false);
 plot_result = get_kwarg(varargin, 'plot', false);
 
 if ~phase_shift
-    
-    s90 = @(phi) sig_x * sin(phi) + sig_y * cos(phi);
-    sig90amp = @(phi) sum(abs(s90(phi) - mean(s90(phi))));
 
-    phase_shift = fminbnd(sig90amp, 0, pi);
+    s90 = @(phi) sig_x * sin(phi) + sig_y * cos(phi);
+    sig90amp = @(phi) sum(abs(s90(phi)) - mean(s90(phi)));
+
+    phase_shift = fminbnd(sig90amp, -pi, pi);
 end
 
 s0 = sig_x * cos(phase_shift) + sig_y * sin(phase_shift);
@@ -43,12 +43,12 @@ s90 = sig_x * sin(phase_shift) + sig_y * cos(phase_shift);
 
 if plot_result
     x = 1:length(sig_x);
-    
+
     subplot(2, 1, 1)
     plot(x, sig_x, x, sig_y)
     legend(["Channel X", "Channel Y"])
     grid on; axis tight;
-    
+
     subplot(2, 1, 2)
     plot(x, s0, x, s90)
     legend(["0 deg", "90 deg"])
