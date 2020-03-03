d1ef78ca52b10cad8fb58d64b3011e873d36ee02
Sam Schott <ss2151@cam.ac.uk>
2020-03-03 10:41:40 +0000

    rename input variable to pars

--- +utils/assert_powersat_exp.m
+++ +utils/assert_powersat_exp.m
@@ -1,10 +1,10 @@
-function [] = assert_powersat_exp(input)
+function [] = assert_powersat_exp(pars)
 %Raises an error if the specified Xepr parameters do not correspond to a
 %powersaturation measurement.
 
 import esr_analyses.utils.*
 
-if ~is_powersat_exp(input)
+if ~is_powersat_exp(pars)
     error('The given data is not from a power saturation measurement.');
 end
 
