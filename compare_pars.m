ee220565f5d493367bea1165c254ebfd44d0a39b
Sam Schott <ss2151@cam.ac.uk>
2020-11-04 11:48:22 +0000

    clean up parameter comparison

--- compare_pars.m
+++ compare_pars.m
@@ -27,7 +27,7 @@ end
 
 ParCheckIgnore = {'TITL', 'TIME', 'DATE', 'MWFQ', 'FrequencyMon', ...
          'Flyback', 'XMIN', 'XWID', 'XMAX', 'StaticFieldMon', ...
-         'NbScansAcc', 'NbScansToDo', 'OPER'};
+         'NbScansAcc', 'NbScansToDo', 'OPER', 'Stability'};
 
 if strcmp(dset1.YTYP, 'IGD') == 1
     ParCheckIgnore = [ParCheckIgnore, {'Power', 'PowerAtten', 'MWPW'}];
@@ -36,13 +36,14 @@ end
 ParsNames1 = fieldnames(dset1);
 ParsNames2 = fieldnames(dset2);
 
-N = min(length(ParsNames1), length(ParsNames2));
+ParsNames = intersect(ParsNames1, ParsNames2);
+N = length(ParsNames);
 
 comp = zeros(N, 1);
 
 for i = 1:N
-    if isequal(ParsNames1(i), ParsNames2(i)) && ~StrInList(ParsNames1(i), ParCheckIgnore)
-        comp(i) = isequal(dset1.(ParsNames1{i}), dset2.(ParsNames2{i})) - 1;
+    if ~any(strcmp(ParCheckIgnore, ParsNames{i}))
+        comp(i) = ~isequal(dset1.(ParsNames{i}), dset2.(ParsNames{i}));
     end
 end
 
@@ -53,18 +54,7 @@ if n_diff > 0
     fprintf(2, 'The following parameters are different:\n');
 end
 for i = diff_indices'
-    fprintf('%s:\t%s vs %s\n', ParsNames1{i}, dset1.(ParsNames1{i}), dset2.(ParsNames2{i}));
+    fprintf('%s:\t%s vs %s\n', ParsNames{i}, dset1.(ParsNames{i}), dset2.(ParsNames{i}));
 end
 
 end
-
-
-function out = StrInList(str, list)
-out = 0;
-for j = 1:length(list)
-    if isequal(str, list(j))
-       out = 1;
-    end
-end
-end
-
